2ce7c5901aa1ebb4bb5a9cf932e08aebb86c0c21
PhuXien <pxchan2003@gmail.com>
2026-08-24 17:26:56 +0800

    Echo User Command and Exit on bye

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 /** Entry point for the Edith chatbot. */
 public class Edith {
+    private static final String DIVIDER = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = """
                  _____ ____ ___ _____ _   _
@@ -12,7 +16,21 @@ public class Edith {
         System.out.println(banner);
         System.out.println("Hello! I'm EDITH.");
         System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println(DIVIDER);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(DIVIDER);
+                return;
+            }
+
+            System.out.println(command);
+            System.out.println(DIVIDER);
+        }
     }
 }
