4f360b8f8c0bb08bca221091e46fac62d7694133
PhuXien <pxchan2003@gmail.com>
2026-08-24 17:11:41 +0800

    Update the initial code to simply greet the user and exit.

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -10,5 +10,9 @@ public class Edith {
                 """;
 
         System.out.println(banner);
+        System.out.println("Hello! I'm EDITH.");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
