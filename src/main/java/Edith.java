f4427be0eacd3d7f1a4b422d524b188a5e55e24b
PhuXien <pxchan2003@gmail.com>
2026-08-24 17:04:26 +0800

    Change from Duke to Edith chatbot with corresponding ASCII Art

---
+++ src/main/java/Edith.java
@@ -0,0 +1,14 @@
+/** Entry point for the Edith chatbot. */
+public class Edith {
+    public static void main(String[] args) {
+        String banner = """
+                 _____ ____ ___ _____ _   _
+                | ____|  _ \\_ _|_   _| | | |
+                |  _| | | | | |  | | | |_| |
+                | |___| |_| | |  | | |  _  |
+                |_____|____/___| |_| |_| |_|
+                """;
+
+        System.out.println(banner);
+    }
+}
